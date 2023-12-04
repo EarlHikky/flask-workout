babd4b937a97f71a8de7af839a0cfecdc2a69a26
Earl <earlhikky91@gmail.com>
2023-12-04 11:31:23 +0300

    Использование шаблонов страниц сайта

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+menu = ["Установка", "Первое приложение", "Обратная связь"]
 
-@app.route("index")
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/add_exercise')
+def add_exercise():
+    return render_template('add_exercise.html', title='Add Exercise', menu = menu)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
