2a76f8ebb608a7da045d68c3ea3d147b0c19f762
Earl <earlhikky91@gmail.com>
2023-12-04 12:38:56 +0300

    Мгновенные сообщения - flash, get_flashed_messages

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
-menu = ['Новая тренировка', 'Список тренировок', 'Добавить упражнение']
+app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
+menu = [{"name": "Новая тренировка", "url": "new-workout"},
+        {"name": "Список тренировок", "url": "workouts"},
+        {"name": "Добавить упражнение", "url": "add-exercise"}]
 
 
-@app.route('/add_exercise')
-def add_exercise():
-    return render_template('add_exercise.html', title='Add Exercise', menu=menu)
+# @app.route('/add-exercise')
+# def add_exercise():
+#     return render_template('add_exercise.html', title='Add Exercise', menu=menu)
 
 
 # @app.route("/profile/<int:username>/<path>")
@@ -17,10 +20,22 @@ def user(username):
     return f'Пользователь: {username}'
 
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('user', username="selfedu"))
+@app.route('/add-exercise', methods=['POST', 'GET'])
+def add_exercise():
+    if request.method == 'POST':
+        print(request.form['title'])
+        if len(request.form['title']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('add_exercise.html', title='Добавить упражнение', menu=menu)
+
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('user', username="selfedu"))
 
 if __name__ == '__main__':
     app.run(debug=True)
