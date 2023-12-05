567763928f567536ed965f627db4aa72f6a0662e
Earl <earlhikky91@gmail.com>
2023-12-05 18:29:28 +0300

    Создание схемы в SQLAlchemy ORM

--- db.py
+++ db.py
@@ -1,38 +1,48 @@
 from datetime import datetime
 
+from sqlalchemy.orm import declarative_base
+
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, Text, ForeignKey, DateTime, Time
+from sqlalchemy.orm import relationship
 
 # Подключение к серверу PostgreSQL на localhost с помощью psycopg2 DBAPI
 engine = create_engine('postgresql+psycopg2://postgres@localhost/workouts')
 engine.connect()
 
-metadata = MetaData()
+Base = declarative_base()
 
-user = Table('users', metadata,
-             Column('id', Integer(), primary_key=True),
-             Column('name', String(25), nullable=False),
-             )
 
-workouts = Table('workouts', metadata,
-                 Column('id', Integer(), primary_key=True),
-                 Column('date', DateTime(), default=datetime.now),
-                 Column('duration', Time()),
-                 Column('user_id', ForeignKey('users.id')),
-                 )
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25), nullable=False)
 
-sets = Table('sets', metadata,
-             Column('id', Integer(), primary_key=True),
-             Column('reps', Integer()),
-             Column('weight', Float()),
-             Column('workout_id', ForeignKey('workouts.id')),
-             Column('exercise_id', ForeignKey('exercises.id')),
-             Column('duration', Time()),
-             Column('rest', Time()),
-             )
 
-exercises = Table('exercises', metadata,
-                  Column('id', Integer(), primary_key=True),
-                  Column('title', String(100), nullable=False),
-                  )
+class Exercise(Base):
+    __tablename__ = 'exercises'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
 
-metadata.create_all(engine)
+
+class Workout(Base):
+    __tablename__ = 'workouts'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime(), default=datetime.now)
+    duration = Column(Time())
+    user_id = Column(Integer, ForeignKey('users.id'))
+    sets = relationship('Sets', backref='workout')
+
+
+class Sets(Base):
+    __tablename__ = 'sets'
+    id = Column(Integer, primary_key=True)
+    reps = Column(Integer())
+    weight = Column(Float())
+    workout_id = Column(Integer, ForeignKey('workouts.id'))
+    exercise_id = Column(Integer, ForeignKey('exercises.id'))
+    exercise = relationship('Exercise')
+    duration = Column(Time())
+    rest = Column(Time())
+
+
+Base.metadata.create_all(engine)
